c2bed5703cad8c29dc190e0b6a73285a8d939f89
nijadakacmoli <84542014+nijadakacmoli@users.noreply.github.com>
2023-09-14 17:09:13 +0200

    Create 1650B.cpp
    B. DIV + MOD
    
    Not so long ago, Vlad came up with an interesting function:
    𝑓𝑎(𝑥)=⌊𝑥𝑎⌋+𝑥mod𝑎, where ⌊𝑥𝑎⌋ is 𝑥𝑎, rounded down, 𝑥mod𝑎 — the remainder of the integer division of 𝑥 by 𝑎. For example, with 𝑎=3 and 𝑥=11, the value 𝑓3(11)=⌊113⌋+11mod3=3+2=5. The number 𝑎 is fixed and known to Vlad. Help Vlad find the maximum value of 𝑓𝑎(𝑥) if 𝑥 can take any integer value from 𝑙 to 𝑟 inclusive (𝑙≤𝑥≤𝑟).

---
+++ 1650B.cpp
@@ -0,0 +1,85 @@
+#include <atomic>
+#include <climits>
+#include <cstdio>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <cstring>
+#include <cmath>
+#include <ratio>
+#include <string>
+#include <sys/types.h>
+#include <type_traits>
+#include <vector>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <deque>
+#include <list>
+#include <iterator>
+#include <utility>
+#include <errno.h>
+#include <chrono>
+#include <cmath>
+#define YES cout<<"YES"<<endl
+#define SWAP(x, y, T) do { T z = x; x = y; y = z; } while (0)
+#define NO cout<<"NO"<<endl
+#define bin_to_int(BIN_STR) stoi(BIN_STR, 0, 2); //binary string to the integer
+#define all(x) x.begin(), x.end()
+using namespace std;
+typedef long long int ll;
+const ll MOD = 1000000007;
+typedef vector<int> vi;
+typedef deque<int> di;
+typedef vector<ll> vl;
+typedef set<int> set_inc;
+typedef set<int, greater<int> > set_dec;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pl;
+typedef map<int,int> mi;
+typedef map<ll,ll> ml; 
+typedef vi::iterator vi_it;
+typedef mi::iterator mi_it;
+ 
+ 
+using namespace std;
+ 
+void solve(){
+    int l,r,a;
+    cin >> l;
+    cin >> r;
+    cin >> a;
+ 
+    int answer=r/a + r%a;
+    int m = r/a*a-1;
+    if(m>=l){
+        answer=max(answer,m/a+m%a);
+    }
+ 
+    cout << answer << endl;
+ 
+ 
+    //cout << *max_element(answer.begin(),answer.end()) << endl;
+ 
+}   
+int main(){
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+    ll t=1;
+    int i=1;
+    cin>>t;
+    while(t--){
+        //cout<<"Case #"<<i<<": ";
+    
+        solve();
+        i++;
+    }
+    return 0;
+}
+ 
+//nijada kacmoli
